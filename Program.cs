2a994552324a24cbf37789afe2aa296f740d04a9
GordIsai857 <GordIsai857@2260I-L29-014>
2022-09-28 09:40:27 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace logbug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> veges=new List<string>();
+            veges.Add("CARROT");
+            veges.Add("BEET");
+            veges.Add("CELERY");
+            veges.Add("RADDISH"); 
+            veges.Add("RADISH");
+            veges.Add("CABBAGE");
+            Random randnum=new Random();
+            int i=25;
+            int[] nums = new int[i];
+            for (i = 0; i < nums.Length; i++)
+            { 
+                nums[i] = (randnum.Next(10, 21));
+                Console.Write($"{nums[i]},");
+            }
+            Options();
+        }
+        class Chosen
+        {
+            public int Lite;
+        }
+        static void Options()
+        {
+            
+        }
+    }
+}
